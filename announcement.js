f5559e339b4e75b96b64e38678eb1bcda7eb8a82
John Marc Robles <2024200433@ms.bulsu.edu.ph>
2026-02-26 16:48:34 +0800

    Initial Commit

---
+++ announcement.js
@@ -0,0 +1,38 @@
+let announcements = [];
+
+function validator() {
+    console.log("Validator function called");
+    const title = document.getElementById("Title").value;
+    const content = document.getElementById("Paragraph").value;
+
+    if (!title|| !content) {
+        alert("Please fill in all fields.");
+        return;
+    } else {
+        updateAnnouncement(title, content);
+    }
+}
+
+function updateAnnouncement(title, content) {
+    const newPost = {
+        title: title,
+        content: content
+    };
+    announcements.push(newPost);
+    render(announcements);
+}
+
+function render(announcements) {
+    const container = document.getElementById("announcement-container");
+    container.innerHTML = '';
+    announcements.forEach(post => {
+        container.innerHTML += `
+                <div class="announcement-container">
+                <h1>${post.title}</h1>
+                <p>${post.content}</p>
+            </div>
+        `;
+    });
+
+    console.log(announcements);
+}
